d29cab0cf3ca093a434c4a767c7760d465d29c42
woworker <570435050@qq.com>
2018-04-17 18:20:33 +0800

    20180417 二叉树后续非递归

--- lianbiao/lianbiao/字母异位词-考察字符加减.cpp
+++ lianbiao/lianbiao/字母异位词-考察字符加减.cpp
@@ -6,33 +6,33 @@
 using namespace std;
 
 
-int _tmain(int argc, _TCHAR* argv[]){
-	string s1,s2;
-	getline(cin,s1);
-	getline(cin,s2);
-
-
-	if (s1.length()!=s2.length())
-	{
-		cout<<"false"<<endl;
-		return 0;
-	}
-	vector<int> word(26,0);
-	
-	for (int i=0;i<s1.length();i++)
-	{
-		word[s1[i]-'a']++;
-		word[s2[i]-'a']--;
-	}
-	for (auto a:word)
-	{
-		if(a!=0)
-			cout<<"false" ;
-	}
-	cout<<"true";
-	
-	system("pause");
-	getchar();
-	return 1;
-
-}
+//int _tmain(int argc, _TCHAR* argv[]){
+//	string s1,s2;
+//	getline(cin,s1);
+//	getline(cin,s2);
+//
+//
+//	if (s1.length()!=s2.length())
+//	{
+//		cout<<"false"<<endl;
+//		return 0;
+//	}
+//	vector<int> word(26,0);
+//	
+//	for (int i=0;i<s1.length();i++)
+//	{
+//		word[s1[i]-'a']++;  //ÕæÌØÂë ¾øÁË
+//		word[s2[i]-'a']--;
+//	}
+//	for (auto a:word)
+//	{
+//		if(a!=0)
+//			cout<<"false" ;
+//	}
+//	cout<<"true";
+//	
+//	system("pause");
+//	getchar();
+//	return 1;
+//
+//}
